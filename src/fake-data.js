2f66555ae8b3a65926757b766550d3e80a5af374
twit <fsmart2006@gmail.com>
2018-08-18 21:23:08 +0300

    added some fake data stuff

---
+++ src/fake-data.js
@@ -0,0 +1,16 @@
+const fakeData = {
+    toDos: {
+        'todo-1': { id: 'todo-1', content: "my todo 1"},
+        'todo-2': { id: 'todo-2', content: "my todo 2"},
+        'todo-3': { id: 'todo-3', content: "my todo 3"},
+        'todo-4': { id: 'todo-4', content: "my todo 4"}  ,       
+    },
+    columns: {
+        'column-1': {
+            id: 'column-1',
+            title: 'My toDos',
+            toDosIds: ['todo-1', 'todo-2', 'todo-3', 'todo-4'],
+        },
+    },
+    columnOrder: ['column-1']
+};
